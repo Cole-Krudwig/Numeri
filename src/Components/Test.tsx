93e75bdfbac5e58c5c9530dd66547fd9abec28c5
Cole Krudwig <ckrudwig@gmail.com>
2023-12-11 16:48:06 -0600

    init: Initialize Repository

---
+++ src/Components/Test.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const SimpleCard = () => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+      <img
+        className="w-full"
+        src="https://via.placeholder.com/350x150"
+        alt="Placeholder"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">Card Title</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
+          purus feugiat.
+        </p>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+          #Tag3
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default SimpleCard;
