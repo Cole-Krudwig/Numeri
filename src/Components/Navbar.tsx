1ccc53e39bec4a363ab6084138eed162f794fa6f
Cole Krudwig <ckrudwig@gmail.com>
2023-12-17 13:53:13 -0600

    Installed hamburger library

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Divide as Hamburger } from "hamburger-react";
 
 interface Tab {
   id: number;
@@ -17,22 +18,69 @@ const Navbar: React.FC<NavbarProps> = ({
   activeIndex,
   onTabClick,
 }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div>
-      <div className="bg-red-400 flex justify-center space-x-16 px-4 py-4">
-        {tabs.map((tab) => (
-          <button
-            key={tab.id}
-            className={`${
-              activeIndex === tab.id
-                ? "text-white border-b-2 border-white"
-                : "hover:text-white hover:border-b-2 hover:border-white"
-            }`}
-            onClick={() => onTabClick(tab.id)}
-          >
-            {tab.title}
-          </button>
-        ))}
+      <div className="bg-red-400 px-4 py-4">
+        <div className="flex justify-between items-center">
+          {/* Logo or Branding can be added here */}
+
+          {/* Hamburger Menu Icon */}
+          <div className="lg:hidden cursor-pointer" onClick={toggleMenu}>
+            <Hamburger toggled={isMenuOpen} toggle={setIsMenuOpen} />
+          </div>
+
+          {/* Navigation Links - Shown on larger screens */}
+          <div className="hidden lg:flex space-x-8">
+            {tabs.map((tab) => (
+              <button
+                key={tab.id}
+                className={`${
+                  activeIndex === tab.id
+                    ? "text-white border-b-2 border-white"
+                    : "hover:text-white hover:border-b-2 hover:border-white"
+                }`}
+                onClick={() => {
+                  onTabClick(tab.id);
+                  closeMenu(); // Close the menu when a tab is clicked on smaller screens
+                }}
+              >
+                {tab.title}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        {/* Responsive Hamburger Menu */}
+        {isMenuOpen && (
+          <div className="lg:hidden mt-4">
+            {tabs.map((tab) => (
+              <button
+                key={tab.id}
+                className={`block w-full py-2 ${
+                  activeIndex === tab.id
+                    ? "bg-gray-700 text-white"
+                    : "hover:bg-gray-300"
+                }`}
+                onClick={() => {
+                  onTabClick(tab.id);
+                  closeMenu();
+                }}
+              >
+                {tab.title}
+              </button>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
